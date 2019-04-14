8c5045e896fd59b3af39b60de7df09cd05fe1fce
HyeongjunCho <hyeongjun@kaist.ac.kr>
2019-04-15 00:10:29 +0900

    Load image from local source

--- app/pages/Home.vue
+++ app/pages/Home.vue
@@ -3,8 +3,8 @@
         <FixedAbsoluteLayout backgroundColor="#eeeeee" width="100%" height="100%">
             <Label class="info" customTop="4.7%" customLeft="61.6%" width="20%" height="6%" backgroundColor="#ff0000">
             </Label>
-            <Image src="../static/image/triangle.png" customTop="50%" customLeft="50%" stretch="none" />
-            <Image src="../static/image/triangle.png" customTop="51%" customLeft="51%" stretch="none" />
+            <Image src="~/static/image/triangle.png" customTop="50%" customLeft="50%" stretch="none" />
+            <Image src="~/static/image/triangle.png" customTop="51%" customLeft="51%" stretch="none" />
         </FixedAbsoluteLayout>
     </Page>
 </template>
