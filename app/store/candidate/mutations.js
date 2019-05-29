79b784f39c86dbb3e502705b28e79e0275fa5353
HyeongjunCho <hyeongjun@kaist.ac.kr>
2019-05-29 19:31:55 +0900

    Make initializing function about canditate

--- app/store/candidate/mutations.js
+++ app/store/candidate/mutations.js
@@ -1,5 +1,14 @@
 const mutations = {
-
+    initializeCandidates: function (state) {
+        const { candidates, candidateTemplate } = state;
+        for (let i = 1; i < 13; i++) {
+            const candidate = JSON.parse(JSON.stringify(candidateTemplate));
+            //
+            // set status value
+            //
+            candidates[i] = candidate;
+        }
+    },
 };
 
 export default mutations;
